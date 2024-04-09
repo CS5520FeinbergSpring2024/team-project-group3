2f5eb64e8ea725ac634b2a5fc04ee0df534c4384
sabelakhoua <belakhoua.s@northeastern.edu>
2024-04-09 18:14:46 +0100

    April 9th Update

--- app/src/main/java/com/example/final_project/RegisterActivity.java
+++ app/src/main/java/com/example/final_project/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.final_project;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
-
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText editTextEmail, editTextPassword, editTextUsername;
@@ -25,17 +25,21 @@ public class RegisterActivity extends AppCompatActivity {
     private Button buttonRegister;
     private FirebaseAuth auth;
     private FirebaseFirestore firestore;
+    private ProgressDialog progressDialog; // Add this line
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        FirebaseApp.initializeApp( this);
+
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        FirebaseApp.initializeApp(this);
         FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance());
+        progressDialog = new ProgressDialog(this); // Initialize the ProgressDialog
 
-        firebaseAppCheck.installAppCheckProviderFactory(
-                PlayIntegrityAppCheckProviderFactory.getInstance());
-
+        // Initialization of UI components
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         editTextUsername = findViewById(R.id.editTextUsername);
@@ -43,15 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
         radioButtonPetOwner = findViewById(R.id.radioButtonPetOwner);
         buttonRegister = findViewById(R.id.buttonRegister);
 
-        auth = FirebaseAuth.getInstance();
-        firestore = FirebaseFirestore.getInstance();
-
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerUser();
-            }
-        });
+        buttonRegister.setOnClickListener(view -> registerUser());
     }
 
     private void registerUser() {
@@ -60,27 +56,23 @@ public class RegisterActivity extends AppCompatActivity {
         String username = editTextUsername.getText().toString().trim();
         String role = radioButtonShelterOwner.isChecked() ? "shelterOwner" : "petOwner";
 
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    String userId = auth.getCurrentUser().getUid();
-                    firestore.collection("Users").document(userId).set(
-                            new User(username, email, role)
-                    ).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-                                finish();
-                            } else {
-                                Toast.makeText(RegisterActivity.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(RegisterActivity.this, "Authentication failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                }
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show(); // Show the ProgressDialog when registration starts
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            progressDialog.dismiss(); // Dismiss the ProgressDialog when registration completes
+            if (task.isSuccessful()) {
+                String userId = auth.getCurrentUser().getUid();
+                firestore.collection("Users").document(userId).set(new User(username, email, role))
+                        .addOnSuccessListener(aVoid -> {
+                            Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_LONG).show();
+                            Intent intent = role.equals("shelterOwner") ? new Intent(RegisterActivity.this, ShelterOwnerDashboardActivity.class) : new Intent(RegisterActivity.this, PetOwnerViewActivity.class);
+                            startActivity(intent);
+                            finish();
+                        })
+                        .addOnFailureListener(e -> Toast.makeText(RegisterActivity.this, "Failed to register user", Toast.LENGTH_LONG).show());
+            } else {
+                Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
             }
         });
     }
