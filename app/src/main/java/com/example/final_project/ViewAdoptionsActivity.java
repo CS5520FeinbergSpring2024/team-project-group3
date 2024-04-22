448d327d9e9c909231ed8e92d5ff19d83f7f4dc3
Anna Brunkhorst <abrunkhorst510@gmail.com>
2024-04-21 21:54:51 -0400

    ViewAdoptions crash fix, login layout adjustment

--- app/src/main/java/com/example/final_project/ViewAdoptionsActivity.java
+++ app/src/main/java/com/example/final_project/ViewAdoptionsActivity.java
@@ -3,12 +3,18 @@ package com.example.final_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,8 +22,7 @@ import java.util.List;
 public class ViewAdoptionsActivity extends AppCompatActivity {
     private RecyclerView applicationsRecyclerView;
     private AdoptionApplicationAdapter adapter;
-    private FirebaseFirestore firestore;
-
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,34 +31,39 @@ public class ViewAdoptionsActivity extends AppCompatActivity {
 
         applicationsRecyclerView = findViewById(R.id.applicationsRecyclerView);
         applicationsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         adapter = new AdoptionApplicationAdapter(new ArrayList<>());
         applicationsRecyclerView.setAdapter(adapter);
 
-        firestore = FirebaseFirestore.getInstance();
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         fetchAdoptionApplications();
     }
 
     private void fetchAdoptionApplications() {
         Intent intent = getIntent();
-        String currentUserId = intent.getData().getUserInfo();
-        firestore.collection("AdoptionApplications")
-                .whereEqualTo("shelterId", currentUserId)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        List<AdoptionApplication> applications = new ArrayList<>();
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            AdoptionApplication application = document.toObject(AdoptionApplication.class);
-                            applications.add(application);
+        String currentUserId = intent.getStringExtra("UserID");
+        if (currentUserId != null) {
+            databaseReference.child("AdoptionApplications")
+                    .orderByChild("shelterId").equalTo(currentUserId)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            List<AdoptionApplication> applications = new ArrayList<>();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                AdoptionApplication application = snapshot.getValue(AdoptionApplication.class);
+                                applications.add(application);
+                            }
+                            adapter.setAdoptionApplications(applications);
                         }
-                        adapter.setAdoptionApplications(applications);
-                    } else {
-                        Log.e("ViewAdoptionsActivity", "Error getting documents: ", task.getException());
-                    }
-                });
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Log.e("ViewAdoptionsActivity", "Error loading applications: ", databaseError.toException());
+                            Toast.makeText(ViewAdoptionsActivity.this, "Error fetching applications from database.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "User ID not found.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
-
